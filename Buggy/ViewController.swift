299c8290f68eb31977387b1d6156763c2c041cf5
Darran Kelinske <darran.kelinske@gmail.com>
2018-06-06 17:51:49 -0500

    first commit

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -19,7 +19,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UISwitch) {
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.)")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i )
+        }
+        
+        for _ in 0...10 {
+            array.removeObject(at: 0)
+        }
+    }
 }
 
